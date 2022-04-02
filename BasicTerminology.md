1df1a62178bb491799061aa4852589a2a0a40513
Somanshu <somanshusharma611@gmail.com>
2022-04-02 18:48:19 +0530

    Added BasicTerminology.md and DepthOfBinaryTree.cpp

---
+++ BasicTerminology.md
@@ -0,0 +1,7 @@
+
+
+
+## Difference between Auxillary Space and Space Complexity  : 
+- **Auxiliary Space** is the temporary space allocated by your algorithm to solve the problem, with respect to input size.
+
+- **Space Complexity** is the total space used by your algorithm to solve the problem, with respect to input size. Note that the Space Complexity includes input size
---
+++ Binary_Trees/DepthOfBinaryTree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        int l = 0,r=0;
+        if((root->left)!=NULL) l = maxDepth(root->left);
+        if((root->right)!=NULL) r = maxDepth(root->right);
+        return max(l,r)+1;
+    }
+};
+
+// Level Order Traversal Approach : 
+// If we use this approach, then space complexity will be O(n/2)=O(n),
+// because at last level of complete binary tree, no. of nodes will be n/2.
+// Complete binary tree is the worst case for this approach.
+
+// Recursive Approach : 
+// This will use Auxillary space of O(log(n)), but in case of a skewed tree, 
+// this too will take O(n) space, however, this is not the case most often as said by
+// striver.
+
+// Both of these approaches have their worst cases or you can say their cons. 
