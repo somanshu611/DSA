402ee01650e03becd63995345c99c1772e85e05b
Somanshu <somanshusharma611@gmail.com>
2022-04-09 22:36:11 +0530

    Added ReverseLinkedList.cpp

---
+++ LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prv = NULL;
+        ListNode* nxt = NULL;
+        ListNode* temp = head;
+        while(temp!=NULL) {
+            nxt = temp->next;
+            if(temp->next == NULL) {
+                head = temp;
+            }
+            temp->next = prv;
+            prv = temp;
+            temp = nxt;
+        }
+        return head;
+    }
+};
